22c1bafb19529a2b2cd6932c3ac9cb11bf4c6b05
egoryhka <egor.demyanenko2000@mail.ru>
2022-11-29 21:43:28 +0300

    Добавьте файлы проекта.

---
+++ CSharpSBTest/UnitTest.cs
@@ -0,0 +1,79 @@
+using CodeCompilerLibrary;
+using CSharpSBTest;
+using NUnit.Framework;
+using System;
+
+namespace CSharpSandboxTests
+{
+    public class Tests
+    {
+
+        public Tests()
+        {
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CodeDiagnosticTest()
+        {
+            CodeDiagnostic(@"");
+        }
+
+        [Test]
+        public void CodeInvocationTest()
+        {
+            CodeInvocation(@"");
+        }
+
+
+        public string[] CodeDiagnostic(string code)
+        {
+            string[] testResults = new[] { "Success" };
+            try
+            {
+                var codeGenerator = new CodeCompiler();
+                var assembly = codeGenerator.CreateAssembly(code);
+                dynamic? instance = assembly.CreateInstance("CSharpSandbox.DynamicClass");
+                int result = instance?.Calc(4);
+            }
+            catch (Exception ex)
+            {
+                if (ex is DiagnosticException diagEx)
+                {
+                    foreach (var mes in diagEx.DiagnosticMessages)
+                        Assert.Warn(mes);
+
+                    testResults = diagEx.DiagnosticMessages;
+                }
+            }
+            return testResults;
+        }
+
+        public string[] CodeInvocation(string code)
+        {
+            string[] testResults = new[] { "Success" };
+            try
+            {
+                var codeGenerator = new CodeCompiler();
+                var assembly = codeGenerator.CreateAssembly(code);
+                dynamic? instance = assembly.CreateInstance("CSharpSandbox.DynamicClass");
+                int result = instance?.Calc(4); // ТУТ под каждый метод нужен свой метод тестирования
+            }
+            catch (Exception ex)
+            {
+                int line = ex.GetLineNumber();
+
+                Assert.Warn(ex.ToString());
+                testResults[0] = (line != -1 ? $"Строка:{line} -> " : "") + ex.Message;
+            }
+            return testResults;
+        }
+
+
+    }
+}
