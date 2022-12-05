ed9cfcdfdfaa64eabae46053b4e1fa952946fce6
ELKOYET <andrey_strelkov@mail.ru>
2022-12-05 17:01:51 +0500

    Добавил вьюшки и контроллеры(пока пустые до консультации с гуриным)

---
+++ CSharpSB/Controllers/LevelController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpSB.Controllers
+{
+    public class LevelController : Controller
+    {
+        public LevelController() 
+        
+        {
+
+        }    
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+
+        [HttpPost]
+        public ActionResult GetHelp(int id) 
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CSharpSB/Services/ValidationService.cs
@@ -0,0 +1,6 @@
+﻿namespace CSharpSB.Services
+{
+    public class ValidationService
+    {
+    }
+}
